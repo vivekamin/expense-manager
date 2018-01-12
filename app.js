2bf31cd87f608d41ba67c3a92cab07352685db01
vivekamin <aminvivek123@gmail.com>
2018-01-12 02:59:16 -0700

    Morgan added for logging and Error handling implemetned

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan')
 
+const expensesRoutes = require('./api/routes/expenses');
+
+app.use(morgan('dev'))
+
+app.use('/expense', expensesRoutes);
 
 app.use((req, res, next) => {
-    res.status(200).json({
-        message: "success"
+    const error = new Error('Not Found');
+    error.status  = 404
+    next(error)
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error : error.message
     });
 });
 
