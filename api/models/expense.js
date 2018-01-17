4ac205a5f39ab4f22f13bb4fbbed87455c289b6f
vivekamin <aminvivek123@gmail.com>
2018-01-17 15:09:06 -0700

    Date property and  Required field option added

--- api/models/expense.js
+++ api/models/expense.js
@@ -2,8 +2,9 @@ const mongoose = require('mongoose');
 
 const expenseSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    name: String,
-    amount: Number
+    name: { type: String, required: true },
+    amount: { type: Number, required: true},
+    date: Date
 });
 
 module.exports = mongoose.model('Expense', expenseSchema);
