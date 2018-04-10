d8e70dbdcc0c6c0bb56c0417c84b23c504e46298
vivekamin <aminvivek123@gmail.com>
2018-04-10 14:57:41 -0700

    changes for heroku

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const http = require('http');
 const app = require('./app');
 
-const process_env = require('./env_var/env');
+//const process_env = require('./env_var/env');
 
 const port = process.env.PORT || 8000;
-console.log(`Running on ${port} and user is ${process_env.USERNAME}`);
+console.log(`Running on ${port} and user is ${process.env.USERNAME}`);
         
 const server = http.createServer(app);
 
